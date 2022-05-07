8e7fa1696f088faeffb21ee23e537c4ce3550341
HuynhThiMinhNhuc <huynhthiminhnhuc@gmail.com>
2022-05-07 10:26:07 +0700

    complete

---
+++ meerapp/lib/present/component/image_card.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:meerapp/config/colorconfig.dart';
+import 'package:meerapp/config/fontconfig.dart';
+import 'package:path/path.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ImageCard extends StatefulWidget {
+  final double width;
+  final double height;
+  final String? hintTitle;
+  final IconData? icon;
+  final Function(File)? onImageChanged;
+  final Function()? onImageDeleted;
+
+  ImageCard({
+    Key? key,
+    this.hintTitle,
+    this.width = 120,
+    this.height = 150,
+    this.onImageChanged,
+    this.onImageDeleted,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  _ImageCardState createState() => _ImageCardState();
+}
+
+class _ImageCardState extends State<ImageCard> {
+  File? imageFile;
+  Future<void> _galleryImage() async {
+    final _picker = ImagePicker();
+    var picture = await _picker.pickImage(source: ImageSource.gallery);
+    if (picture != null) {
+      final directory = await getApplicationDocumentsDirectory();
+
+      //Get image path in storage app
+      final image = File('${directory.path}/${basename(picture.path)}');
+
+      //Create new image from source image, add temporary to storage app
+      final file = await File(picture.path); //.copy(image.path);
+
+      this.setState(() {
+        //Update UI with image
+        imageFile = file;
+      });
+      widget.onImageChanged?.call(imageFile!);
+    }
+  }
+
+  Future<void> _deleteImage() async {
+    if (imageFile != null) {
+      print('request delete if image not null');
+      setState(() {
+        imageFile = null;
+      });
+      widget.onImageDeleted?.call();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = widget.width;
+    final double height = widget.height;
+
+    return Container(
+      child: InkWell(
+        child: imageFile != null
+            ? Stack(children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.file(
+                    imageFile!,
+                    width: width,
+                    height: height,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                    top: 0,
+                    right: 0,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.cancel_outlined,
+                        color: Color(0xff455154),
+                      ),
+                      onPressed: _deleteImage,
+                    ))
+              ])
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (widget.hintTitle != null)
+                    Text(widget.hintTitle!,
+                        textAlign: TextAlign.center,
+                        style: kText13RegularNote),
+                  if (widget.icon != null)
+                    Icon(widget.icon, color: meerColorMain, size: 25),
+                ],
+              ),
+        onTap: _galleryImage,
+        onLongPress: _deleteImage,
+      ),
+      height: 150,
+      width: 120,
+      decoration: BoxDecoration(
+        color: meerColorBackground,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.fromLTRB(10.h, 10.w, 5.h, 10.h),
+    );
+  }
+}
--- meerapp/lib/present/page/new_campaign_page/create_new_campaign_page.dart
+++ meerapp/lib/present/page/new_campaign_page/create_new_campaign_page.dart
@@ -1,186 +1,297 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:meerapp/config/colorconfig.dart';
 import 'package:meerapp/config/fontconfig.dart';
+import 'package:meerapp/constant/user.dart';
+import 'package:meerapp/present/component/image_card.dart';
 
 class CreateNewCampaignPage extends StatelessWidget {
-  const CreateNewCampaignPage({ Key? key }) : super(key: key);
+  const CreateNewCampaignPage({Key? key}) : super(key: key);
+
+  static const List<String> _userName = <String>[
+    'aardvark',
+    'bobcat',
+    'chameleon',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: meerColorBackground,
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: meerColorBlackIcon, //change your color here
-        ),
-        centerTitle: true,
-        backgroundColor: meerColorBackground,
-        title: Text(
-          "Tạo chiến dịch",
-          style: ktext18BoldBlack,
-        ),
-        actions: [
-          TextButton(
-            onPressed: () { },
-            child: Text(
-              "Đăng",
-              style: kText18BoldMain,
-            ),
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
+      appBar: getAppBar(),
+      body: getBody(context),
+    );
+  }
+
+  Padding getBody(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.h),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: meerColorWhite,
+              ),
+              child: Column(
+                children: [
+                  ChoiceField(
+                    icon: Icons.keyboard_arrow_right_outlined,
+                    title: 'Chọn địa điểm',
+                    onPress: () {},
+                  ),
+                  ChoiceField(
+                    icon: Icons.keyboard_arrow_right_outlined,
+                    title: 'Chọn thời gian',
+                    onPress: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(DateTime.now().year - 1,
+                            DateTime.now().month, DateTime.now().day),
+                        lastDate: DateTime(DateTime.now().year + 2),
+                      ).then((value) {
+                        // startDate = value;
+
+                        // if (startDate != null) {
+                        //   final formattedDate =
+                        //       DateFormat('dd/MM/yyyy').format(startDate!);
+                        //   if (formattedDate != _dateTextController.text)
+                        //     setState(() {
+                        //       _dateTextController.text = formattedDate;
+                        //       print("Date selected: $formattedDate");
+                        //     });
+                        // } else {
+                        //   setState(() {
+                        //     _dateTextController.text = '';
+                        //   });
+                        // }
+                      });
+                    },
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(10.h),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+            SizedBox(
+              height: 10.h,
+            ),
+            Container(
+              decoration: BoxDecoration(
                   color: meerColorWhite,
-                ),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ChoiceField(
-                      //controller: _locationTextController,
-                      icon: Icons.keyboard_arrow_right_outlined,
-                      title: 'Chọn địa điểm',
-                      onPress: ()  {},
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Người tham gia",
+                          style: kText15BoldBlack,
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              showDialogAddCampaignUser(context);
+                            },
+                            child: Text(
+                              "Thêm",
+                              style: kText15BoldMain,
+                            ))
+                      ],
                     ),
-                    ChoiceField(
-                     // controller: _dateTextController,
-                      icon: Icons.keyboard_arrow_right_outlined,
-                      title: 'Chọn thời gian',
-                      onPress: () {
-                        showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(DateTime.now().year - 1,
-                              DateTime.now().month, DateTime.now().day),
-                          lastDate: DateTime(DateTime.now().year + 2),
-                        ).then((value) {
-                          // startDate = value;
-
-                          // if (startDate != null) {
-                          //   final formattedDate =
-                          //       DateFormat('dd/MM/yyyy').format(startDate!);
-                          //   if (formattedDate != _dateTextController.text)
-                          //     setState(() {
-                          //       _dateTextController.text = formattedDate;
-                          //       print("Date selected: $formattedDate");
-                          //     });
-                          // } else {
-                          //   setState(() {
-                          //     _dateTextController.text = '';
-                          //   });
-                          // }
-                        });
-                      },
+                    Wrap(
+                      spacing: 8.0.h, // gap between adjacent chips
+                      children: List.generate(
+                        users.length,
+                        (index) => Padding(
+                          padding: EdgeInsets.fromLTRB(5.w, 0, 5.w, 0),
+                          child: Chip(
+                            backgroundColor: meerColorMain,
+                            deleteIcon: const Icon(
+                              FontAwesomeIcons.circleXmark,
+                              color: meerColorWhite,
+                            ),
+                            label: Text(
+                              users[index]["fullName"],
+                              style: TextStyle(
+                                  fontFamily: 'Roboto_Regular',
+                                  fontSize: 13.sp,
+                                  color: meerColorWhite),
+                            ),
+                            onDeleted: () {},
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: const BoxDecoration(
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Container(
+              decoration: const BoxDecoration(
                   color: meerColorWhite,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      TextFormField(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(2.w, 10.h, 0, 10.h),
+                      child: Text(
+                        "  Hình ảnh",
                         style: kText17BoldBlack,
-                        //controller: _nameTextController,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Thêm tiêu đề tại đây"),
                       ),
-                      TextFormField(
-                          keyboardType: TextInputType.name,
-                          //controller: _descriptionTextController,
-                          minLines: 8,
-                          maxLines: 15,
-                          style: TextStyle(
-                              fontFamily: 'Roboto-Regular.ttf',
-                              fontSize: 15.sp,
-                              color: Colors.grey[600]),
-                          decoration: InputDecoration(
-                              hintText: "Viết nội dung ở đây...",
-                              errorBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              border: InputBorder.none,
-                              enabledBorder: InputBorder.none)),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: meerColorWhite,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 15,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width - 20.w,
-                        //  alignment: ,
-                        child: Text(
-                          "Yêu cầu",
-                          style: kText17BoldBlack,
+                    ),
+                    Row(
+                      children: [
+                        ImageCard(
+                          hintTitle: "+ Ảnh bìa",
+                          onImageChanged: (file) {
+                            // backgroundImage = file;
+                          },
                         ),
-                      ),
-                      Container(
-                        height: 10,
-                      ),
-                      TextFormField(
-                          keyboardType: TextInputType.multiline,
-                          //controller: _requireTextController,
-                          minLines: 8,
-                          maxLines: 10,
-                          style: TextStyle(
-                              fontFamily: 'Roboto-Regular.ttf',
-                              fontSize: 15.sp,
-                              color: Colors.grey[600]),
-                          decoration: InputDecoration(
-                              hintText: "Viết yêu cầu ở đây...",
-                              errorBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              border: InputBorder.none,
-                              enabledBorder: InputBorder.none)),
-                    ],
-                  ),
+                        ImageCard(
+                          hintTitle: "+ Ảnh đại diện",
+                          onImageChanged: (file) {
+                            // avatarImage = file;
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: meerColorWhite,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      style: kText17BoldBlack,
+                      //controller: _nameTextController,
+                      decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Thêm tiêu đề tại đây"),
+                    ),
+                    TextFormField(
+                        keyboardType: TextInputType.name,
+                        //controller: _descriptionTextController,
+                        minLines: 15,
+                        maxLines: 15,
+                        style: TextStyle(
+                            fontFamily: 'Roboto-Regular.ttf',
+                            fontSize: 15.sp,
+                            color: Colors.grey[600]),
+                        decoration: const InputDecoration(
+                            hintText: "Viết nội dung ở đây...",
+                            errorBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            border: InputBorder.none,
+                            enabledBorder: InputBorder.none)),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Future<String?> showDialogAddCampaignUser(BuildContext context) {
+    return showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  AlertDialog(
+                                title: Text(
+                                  'Thêm người tham gia',
+                                  style: kText15BoldBlack,
+                                ),
+                                content: Autocomplete<String>(
+                                  optionsBuilder:
+                                      (TextEditingValue textEditingValue) {
+                                    if (textEditingValue.text == '') {
+                                      return const Iterable<String>.empty();
+                                    }
+                                    return _userName.where((String option) {
+                                      return option.contains(
+                                          textEditingValue.text
+                                              .toLowerCase());
+                                    });
+                                  },
+                                  onSelected: (String selection) {
+                                    debugPrint(
+                                        'You just selected $selection');
+                                  },
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Hủy'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'OK'),
+                                    child: const Text('Lưu'),
+                                  ),
+                                ],
+                              ),
+                            );
+  }
+
+  AppBar getAppBar() {
+    return AppBar(
+      iconTheme: const IconThemeData(
+        color: meerColorBlackIcon, //change your color here
+      ),
+      centerTitle: true,
+      backgroundColor: meerColorBackground,
+      title: Text(
+        "Tạo chiến dịch",
+        style: ktext18BoldBlack,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            "Đăng",
+            style: kText18BoldMain,
+          ),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
+
 class ChoiceField extends StatefulWidget {
   final String title;
   final IconData icon;
