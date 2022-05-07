74a0d137304fe551b3e9069369d84825d5c9410d
HuynhThiMinhNhuc <huynhthiminhnhuc@gmail.com>
2022-05-07 16:46:25 +0700

    Add toggle button
    easy

--- meerapp/lib/present/page/map/map_page.dart
+++ meerapp/lib/present/page/map/map_page.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:meerapp/config/colorconfig.dart';
 import 'package:meerapp/config/constant.dart';
+import 'package:meerapp/config/fontconfig.dart';
+import 'package:meerapp/present/page/home_page/detail_campaign_page.dart';
 
 import '../../models/map.dart';
 
@@ -19,6 +23,7 @@ class _MapPageState extends State<MapPage> {
   late CameraPosition initialCameraPosition;
   final CustomInfoWindowController _infoWindowController =
       CustomInfoWindowController();
+  late List<bool> stateToggle =  [true, false];
 
   Marker? currentLocation;
   final Map<IMapObject, Marker> nearEventLocation = <IMapObject, Marker>{};
@@ -98,13 +103,13 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     // ? Mock data
-    // IMapObject mapObject = CampaignMap(
-    //   id: 1,
-    //   position: DefaultLatLng,
-    //   name: 'test vị trí',
-    //   time: DateTime.now(),
-    // );
-    // nearEventLocation.addEntries({mapObject: createMaker(mapObject)}.entries);
+    IMapObject mapObject = CampaignMap(
+      id: 1,
+      position: DefaultLatLng,
+      name: 'test vị trí',
+      time: DateTime.now(),
+    );
+    nearEventLocation.addEntries({mapObject: createMaker(mapObject)}.entries);
 
     return Scaffold(
       body: Stack(
@@ -122,9 +127,56 @@ class _MapPageState extends State<MapPage> {
           ),
           CustomInfoWindow(
             controller: _infoWindowController,
-            height: 50,
+            height: 55,
             width: 150,
             offset: 50,
+          ),
+          Positioned.fill(
+            top: 30.h,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                padding: EdgeInsets.zero,
+                decoration: const BoxDecoration(
+                  color: meerColorBackground,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                child: ToggleButtons(
+                  borderRadius: BorderRadius.circular(30),
+                  fillColor: meerColorMain,
+                  color: meerColorBlack,
+                  selectedColor: meerColorWhite,
+                  onPressed: (index) => setState(() {
+                    stateToggle[index] = ! stateToggle[index];
+                  }),
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 0),
+                      child:  const Text(
+                        "Chiến dịch",
+                        style: TextStyle(
+                          fontFamily: "Roboto",
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 0),
+                      child: const Text(
+                        "Khẩn cấp",
+                        style: TextStyle(
+                          fontFamily: "Roboto",
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                  isSelected: stateToggle,
+                ),
+              ),
+            ),
           )
         ],
       ),
@@ -139,16 +191,20 @@ class _MapPageState extends State<MapPage> {
     var newMarker = Marker(
         markerId: MarkerId(mapObject.id.toString()),
         position: mapObject.position,
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
         onTap: () {
+          //location
           _infoWindowController.addInfoWindow!(
             GestureDetector(
               onTap: () {
-                //TODO: open view post
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return const DetailCampaignPage(mode: Status.nonMember);
+                }));
               },
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: meerColorWhite,
                   borderRadius: BorderRadius.circular(4),
                 ),
                 child: Padding(
@@ -157,7 +213,19 @@ class _MapPageState extends State<MapPage> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       // TODO: Change InfoWindow here
-                      Text("Tên: ${mapObject.name}"),
+                      Text(
+                        "Test the nay duoc hong",
+                        //mapObject.name,
+                        style: kText13BoldBlack.copyWith(
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Text(
+                        "Nhấn để xem chi tiết",
+                        style: kText11RegularHintText,
+                      ),
                     ],
                   ),
                 ),
