7bad9ea46db86e1328e364cc5551fcbbc2dcc746
Dragnell <bang2001vl@outlook.com.vn>
2022-05-09 16:30:32 +0700

    optimize login + signup

--- meerapp/lib/present/page/Login/login_view.dart
+++ meerapp/lib/present/page/Login/login_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:async/async.dart';
 import 'package:meerapp/api/route/auth.dart';
 import 'package:meerapp/config/colorconfig.dart';
+import 'package:meerapp/present/component/my_alert_dialog_2.dart';
 import 'package:meerapp/present/page/Login/register_view.dart';
 import 'package:meerapp/singleton/user.dart';
 
@@ -37,24 +38,39 @@ class _LoginState extends State<LoginPage> {
   }
 
   onSubmit() {
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      _showDialog("Thất bại", "Bạn vẫn chưa điền thông tin");
+    }
+
     setState(() {
       isLoading = true;
     });
     final username = emailController.text.trim();
     final password = passwordController.text.trim();
+
     AuthAPI.login(username, password).then((response) {
       if (response.errorCode != null) {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(const SnackBar(content: Text("Đăng nhập thất bại")));
+        _showDialog("Thất bại", "Tên đăng nhập hoặc mật khẩu sai");
       } else {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(const SnackBar(content: Text("Xin chào")));
+        // ScaffoldMessenger.of(context)
+        //     .showSnackBar(const SnackBar(content: Text("Xin chào")));
       }
 
       setState(() {
         isLoading = false;
       });
     });
+  }
+
+  _showDialog(String title, String message) {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => MyAlertDialog2(
+        content: message,
+        isTwoActions: false,
+        title: title,
+      ),
+    );
   }
 
   @override
