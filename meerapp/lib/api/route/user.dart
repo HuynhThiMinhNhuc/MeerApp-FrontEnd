ad69a450fadbcd4541fed012026d1abcdcf4a541
Dragnell <bang2001vl@outlook.com.vn>
2022-05-09 10:13:43 +0700

    temp 1

--- meerapp/lib/api/route/user.dart
+++ meerapp/lib/api/route/user.dart
@@ -42,39 +42,4 @@ class UserAPI {
       data: formdata,
     );
   }
-
-  static Future<MyResponse> login(String username, String password) async {
-    var response = await myAPIWrapper.post(ServerUrl + "/login",
-        options: Options(headers: {
-          HttpHeaders.contentTypeHeader: "application/json",
-        }),
-        data: jsonEncode(
-          {
-            "account": {
-              "username": username,
-              "password": password,
-            }
-          },
-        ));
-    if (response.errorCode != null) {
-      var auth = AuthData();
-      auth.username = username;
-      auth.password = password;
-      auth.token = response.data["token"];
-      UserSingleton.instance.currentUserId = response.data["userId"];
-      UserSingleton.instance.auth = auth;
-      UserSingleton.instance.isLogined = true;
-    }
-    return response;
-  }
-
-  static Future<MyResponse> signout() async {
-    var response = await myAPIWrapper.postWithAuth(ServerUrl + "/signout");
-    if (response.errorCode != null) {
-      UserSingleton.instance.currentUserId = null;
-      UserSingleton.instance.auth = null;
-      UserSingleton.instance.isLogined = false;
-    }
-    return response;
-  }
 }
