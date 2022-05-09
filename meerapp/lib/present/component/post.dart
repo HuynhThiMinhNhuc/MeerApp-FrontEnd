5bee08861afa5953b56f8a6ad4875c7cb0f0dd60
UIT.19520266 <19520266@gm.uit.edu.vn>
2022-05-09 18:25:55 +0700

    add delete post

--- meerapp/lib/present/component/post.dart
+++ meerapp/lib/present/component/post.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:meerapp/api/MyWrapper.dart';
 import 'package:meerapp/config/fontconfig.dart';
 import 'package:meerapp/config/helper.dart';
+import 'package:meerapp/controllers/controller.dart';
+import 'package:meerapp/injection.dart';
+import 'package:meerapp/present/component/my_alert_dialog_2.dart';
+import 'package:meerapp/present/component/my_alert_dialog_3.dart';
 import 'package:meerapp/present/models/status_compaign.dart';
 import 'package:meerapp/present/models/status_emerency.dart';
 import 'package:meerapp/present/page/home_page/detail_campaign_page.dart';
@@ -16,7 +21,9 @@ import '../page/new_campaign_page/create_new_campaign_page.dart';
 class Post extends StatelessWidget {
   final IPost postData;
   final Function? onDeletePost;
-  bool get isMainPost => UserSingleton.instance.auth != null && UserSingleton.instance.auth!.userId == postData.creator.id;
+  bool get isMainPost =>
+      UserSingleton.instance.auth != null &&
+      UserSingleton.instance.auth!.userId == postData.creator.id;
 
   const Post({
     Key? key,
@@ -48,10 +55,11 @@ class Post extends StatelessWidget {
                     Container(
                       width: 40.h,
                       height: 40.h,
-                      decoration:  BoxDecoration(
+                      decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           image: DecorationImage(
-                              image: MyImageProvider(postData.imageUrl, const AssetImage("asset/avatardefault.png")),
+                              image: MyImageProvider(postData.imageUrl,
+                                  const AssetImage("asset/avatardefault.png")),
                               fit: BoxFit.cover)
                           // NetworkImage(avatarUrl), fit: BoxFit.cover),
                           ),
@@ -106,24 +114,26 @@ class Post extends StatelessWidget {
                       ),
                       value: 1,
                     ),
-                    if (isMainPost) PopupMenuItem(
-                      value: 2,
-                      child: ListTile(
-                        title: Text(
-                          'Chỉnh sửa',
-                          style: kText13RegularBlack,
+                    if (isMainPost)
+                      PopupMenuItem(
+                        value: 2,
+                        child: ListTile(
+                          title: Text(
+                            'Chỉnh sửa',
+                            style: kText13RegularBlack,
+                          ),
                         ),
                       ),
-                    ),
-                    if (isMainPost) PopupMenuItem(
-                      value: 3,
-                      child: ListTile(
-                        title: Text(
-                          'Xóa',
-                          style: kText13RegularBlack,
+                    if (isMainPost)
+                      PopupMenuItem(
+                        value: 3,
+                        child: ListTile(
+                          title: Text(
+                            'Xóa',
+                            style: kText13RegularBlack,
+                          ),
                         ),
                       ),
-                    ),
                   ],
                 ),
               ],
@@ -131,16 +141,20 @@ class Post extends StatelessWidget {
             SizedBox(
               height: 10.h,
             ),
-            postData.bannerUrl != null?
-            Container(
-              height: 400.h,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: MyImageProvider(postData.bannerUrl, const AssetImage("asset/failedimage.png")),
-                  fit: BoxFit.cover, 
-                ),
-              ),
-            ): Container(height: 0,),
+            postData.bannerUrl != null
+                ? Container(
+                    height: 400.h,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: MyImageProvider(postData.bannerUrl,
+                            const AssetImage("asset/failedimage.png")),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                : Container(
+                    height: 0,
+                  ),
             InkWell(
                 child: Padding(
                   padding: EdgeInsets.fromLTRB(10.w, 7.h, 5.w, 7.h),
@@ -203,10 +217,10 @@ class Post extends StatelessWidget {
       Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => postData is CampaignPost
-                ? CreateNewCampaignPage(initData: postData as CampaignPost)
-                : CreateNewEmergencyPage(initData: postData as EmergencyPost),
-                ),
+          builder: (context) => postData is CampaignPost
+              ? CreateNewCampaignPage(initData: postData as CampaignPost)
+              : CreateNewEmergencyPage(initData: postData as EmergencyPost),
+        ),
       )
     };
   }
@@ -226,7 +240,26 @@ class Post extends StatelessWidget {
             ),
           ),
           TextButton(
-            onPressed: () => Navigator.pop(context),
+            onPressed: () async {
+              var controller = sl.get<PostController>();
+              var isSuccess = await controller.DeletePost(
+                  postData is CampaignPost ? 'campaign' : 'emergency',
+                  [postData.id]);
+              if (isSuccess) {
+                showDialog(
+                  context: context,
+                  builder: (_) => const MyAlertDialog2(
+                      title: 'Thông báo', content: 'Xoá bài viết thành công'),
+                );
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (_) => const MyAlertDialog2(
+                      title: 'Lỗi', content: 'Không thể xoá bài viết'),
+                );
+              }
+              // Navigator.pop(context);
+            },
             child: Text('Xóa', style: kText13BoldMain),
           ),
         ],
