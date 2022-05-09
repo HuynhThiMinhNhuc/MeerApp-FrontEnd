167ca0c3d4d55b96ac5cd3c6056c68b679791e31
Dragnell <bang2001vl@outlook.com.vn>
2022-05-09 10:12:57 +0700

    Done with profile

--- meerapp/lib/api/route/user.dart
+++ meerapp/lib/api/route/user.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:meerapp/api/MyWrapper.dart';
 import 'package:meerapp/config/constant.dart';
+import 'package:meerapp/singleton/user.dart';
 
 class UserAPI {
   static Future<MyResponse> getCurrentUserInfo() async {
@@ -37,5 +41,40 @@ class UserAPI {
       ServerUrl + "/user/update",
       data: formdata,
     );
+  }
+
+  static Future<MyResponse> login(String username, String password) async {
+    var response = await myAPIWrapper.post(ServerUrl + "/login",
+        options: Options(headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+        }),
+        data: jsonEncode(
+          {
+            "account": {
+              "username": username,
+              "password": password,
+            }
+          },
+        ));
+    if (response.errorCode != null) {
+      var auth = AuthData();
+      auth.username = username;
+      auth.password = password;
+      auth.token = response.data["token"];
+      UserSingleton.instance.currentUserId = response.data["userId"];
+      UserSingleton.instance.auth = auth;
+      UserSingleton.instance.isLogined = true;
+    }
+    return response;
+  }
+
+  static Future<MyResponse> signout() async {
+    var response = await myAPIWrapper.postWithAuth(ServerUrl + "/signout");
+    if (response.errorCode != null) {
+      UserSingleton.instance.currentUserId = null;
+      UserSingleton.instance.auth = null;
+      UserSingleton.instance.isLogined = false;
+    }
+    return response;
   }
 }
--- meerapp/lib/present/page/profile/Widgets/detail_join.dart
+++ meerapp/lib/present/page/profile/Widgets/detail_join.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:meerapp/api/MyWrapper.dart';
 import 'package:meerapp/api/route/user.dart';
 import 'package:meerapp/config/fontconfig.dart';
 import 'package:meerapp/present/component/EventJoin.dart';
@@ -16,20 +17,89 @@ class DetailJoinWidget extends StatelessWidget {
     required this.status,
   }) : super(key: key);
 
+  String getStatus(int status) {
+    return status == 0
+        ? "đã tạo"
+        : status == 1
+            ? "đã tham gia"
+            : "bỏ tham gia";
+  }
+
+  Future<List<MyResponse>> getDataList(int status) {
+    if (status == 0) {
+      return Future.wait([
+        UserAPI.getCreatedCampaign(),
+        UserAPI.getCreatedEmergency(),
+      ]);
+    } else if (status == 1) {
+      return Future.wait([
+        UserAPI.getDonedCampaign(),
+        UserAPI.getDonedEmergency(),
+      ]);
+    } else {
+      return Future.wait([
+        UserAPI.getNotDoneCampaign(),
+      ]);
+    }
+  }
+
+  Widget _buildItem(dynamic itemData) {
+    return EventJoinItem(
+        userName: itemData['creator']["fullname"].toString(),
+        title: itemData['title'].toString(),
+        time: itemData['createdAt'].toString(),
+        status: status);
+  }
+
+  Widget _buildBody(
+      BuildContext context, int statusCode, List<List<dynamic>> dataLists) {
+    if (status == 2) {
+      if (dataLists.length != 1) return const Center();
+      final campaignList = dataLists[0];
+      return ListView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: campaignList.length,
+        itemBuilder: (BuildContext context, int index) =>
+            _buildItem(campaignList[index]),
+      );
+    } else {
+      if (dataLists.length != 2) return const Center();
+      final campaignList = dataLists[0];
+      final emergencyList = dataLists[1];
+      return Column(
+        children: [
+          ExpansionTile(
+            title: const Text(
+              'Chiến dịch tình nguyện',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              'Gồm các chiến dịch tình nguyện ' + getStatus(status),
+              style: kText13RegularNote,
+            ),
+            children: List.generate(campaignList.length,
+                (index) => _buildItem(campaignList[index])),
+          ),
+          ExpansionTile(
+            title: const Text(
+              'Sự kiện khẩn cấp',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              'Gồm các sự kiện khẩn cấp ' + getStatus(status),
+              style: kText13RegularNote,
+            ),
+            children: List.generate(emergencyList.length,
+                (index) => _buildItem(emergencyList[index])),
+          ),
+        ],
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var future = Future.wait([
-      UserAPI.getDonedCampaign(),
-      UserAPI.getDonedEmergency(),
-    ]);
-    String getStatus(int status) {
-      return status == 0
-          ? "đã tạo"
-          : status == 1
-              ? "đã tham gia"
-              : "bỏ tham gia";
-    }
-
     return SingleChildScrollView(
       child: Column(
         mainAxisSize: MainAxisSize.max,
@@ -37,63 +107,24 @@ class DetailJoinWidget extends StatelessWidget {
           SizedBox(
             height: 15.h,
           ),
-          status == 2
-              ? ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: campaignList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return EventJoinItem(
-                        userName: campaignList[index]['name'].toString(),
-                        postImageUrl:
-                            campaignList[index]['postImageUrl'].toString(),
-                        title: campaignList[index]['title'].toString(),
-                        time: campaignList[index]['time'].toString(),
-                        status: status);
-                  },
-                )
-              : Column(
-                  children: [
-                    ExpansionTile(
-                      title: const Text(
-                        'Chiến dịch tình nguyện',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Gồm các chiến dịch tình nguyện ' + getStatus(status),
-                        style: kText13RegularNote,
-                      ),
-                      children: List.generate(
-                          campaignList.length,
-                          (index) => EventJoinItem(
-                              userName: campaignList[index]['name'].toString(),
-                              postImageUrl: campaignList[index]['postImageUrl']
-                                  .toString(),
-                              title: campaignList[index]['title'].toString(),
-                              time: campaignList[index]['time'].toString(),
-                              status: status)),
-                    ),
-                    ExpansionTile(
-                      title: const Text(
-                        'Sự kiện khẩn cấp',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Gồm các sự kiện khẩn cấp ' + getStatus(status),
-                        style: kText13RegularNote,
-                      ),
-                      children: List.generate(
-                          campaignList.length,
-                          (index) => EventJoinItem(
-                              userName: campaignList[index]['name'].toString(),
-                              postImageUrl: campaignList[index]['postImageUrl']
-                                  .toString(),
-                              title: campaignList[index]['title'].toString(),
-                              time: campaignList[index]['time'].toString(),
-                              status: status)),
-                    ),
-                  ],
-                ),
+          FutureBuilder<List<MyResponse>>(
+              future: getDataList(status),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final responses = snapshot.data;
+                final dataLists =
+                    snapshot.data!.map((e) => e.data as List<dynamic>).toList();
+                return _buildBody(
+                  context,
+                  status,
+                  dataLists,
+                );
+              })
         ],
       ),
     );
